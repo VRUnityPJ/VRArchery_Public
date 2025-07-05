3a2b0031e4c0ce6d8e9ce8e405d639587f869e3e
nattohikoki <nattohikoki@gmail.com>
2025-07-05 22:25:37 +0900

    add: #5 弓矢とプレイヤーのスクリプトを追加した

--- Assets/_VRArchery/Scripts/Player/PlayerComtroller.cs
+++ Assets/_VRArchery/Scripts/Player/PlayerComtroller.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class PlayerComtroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _camera;
+    private Vector3 _cameraRot;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _cameraRot.x -= Input.GetAxis("Mouse Y");
+        _cameraRot.y += Input.GetAxis("Mouse X");
+
+        _camera.transform.rotation = Quaternion.Euler(_cameraRot);
     }
 }
