7005ad8088aa7401038605266a3d1a535de4bd3b
IMMizuki <t233102@st.yamagata-u.ac.jp>
2025-07-13 15:28:00 +0900

    左コントローラーの回転を弓に反映

--- Assets/Scripts/FixingBowWhenTriggered.cs
+++ Assets/Scripts/FixingBowWhenTriggered.cs
@@ -3,34 +3,43 @@ using UnityEngine.InputSystem;
 
 public class FixingBowWhenTriggered : MonoBehaviour
 {
-    [SerializeField] private GameObject bow;
-    [SerializeField] private InputActionReference leftIndexTriggerAction;
+    [SerializeField] private GameObject _bow;
+    [SerializeField] private InputActionReference _leftIndexTriggerAction;
 
-    private Vector3 initialLocalBowPosition;
-    private Quaternion initialLocalBowRotation;
+    private Vector3 _initialLocalBowPosition;
+    private Quaternion _initialLocalBowRotation;
 
     void Start()
     {
         // Bowの初期位置と回転を保存
-        initialLocalBowPosition = bow.transform.localPosition;
-        initialLocalBowRotation = bow.transform.localRotation;
+        _initialLocalBowPosition = _bow.transform.localPosition;
+        _initialLocalBowRotation = _bow.transform.localRotation;
     }
 
     void Update()
     {
         // Left Triggerが押されっぱなしの時
-        if (leftIndexTriggerAction.action.IsPressed())
+        if (_leftIndexTriggerAction.action.IsPressed())
         {
-            // Bowの位置と回転をその場に固定（Left Controllerとの親子関係を解除）
-            bow.transform.parent = null;
+            // Bowがまだ子オブジェクトの場合、親子関係を一度だけ解除して位置を固定
+            if (_bow.transform.parent != null)
+            {
+                _bow.transform.parent = null;
+            }
+
+            // 左コントローラーの回転をBowに反映
+            _bow.transform.rotation = this.transform.rotation;
         }
         // Left Triggerが押されてない時
         else
         {
-            // Bowの位置と回転を初期位置に戻す（Left Controllerとの親子関係を再設定）
-            bow.transform.parent = this.transform;
-            bow.transform.localPosition = initialLocalBowPosition;
-            bow.transform.localRotation = initialLocalBowRotation;
+            // Bowが親子関係にない場合、関係を再設定する
+            if (_bow.transform.parent == null)
+            {
+                _bow.transform.parent = this.transform;
+                _bow.transform.localPosition = _initialLocalBowPosition;
+                _bow.transform.localRotation = _initialLocalBowRotation;
+            }
         }
     }
 }
