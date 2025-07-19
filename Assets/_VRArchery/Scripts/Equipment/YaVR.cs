5eb2895e5fce6fd6d840ca37302492e7cae90c11
nattohikoki <nattohikoki@gmail.com>
2025-07-19 19:40:54 +0900

    feat: #8 矢を飛ばせるようにした

---
+++ Assets/_VRArchery/Scripts/Equipment/YaVR.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class YaVR : MonoBehaviour
+{
+    private bool _isFlying ;
+    private bool _isNocking = false;
+    private bool _canNock = false;
+    private Vector3 _prePosition;
+    private Rigidbody _rb;
+    private BoxCollider _boxCollider;
+    [SerializeField] private GameObject _arrow;
+    [SerializeField] private float _speed = 40;
+    /// <summary>
+    /// 矢をつがえる位置の当たり判定
+    /// </summary>
+    [SerializeField] private GameObject ArrowGrip;
+    [SerializeField] YaFlyingManager yaFlyingManager;
+    [SerializeField] private InputActionAsset _actionAsset;
+    [SerializeField] private XRGrabInteractable _grabInteract;
+    private InputAction nockAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        var actionMap = _actionAsset.FindActionMap("Archery");
+        if (actionMap != null)
+        {
+            nockAction = actionMap.FindAction("nockArrow", true);
+            Debug.Log("nock arrow attached");
+            if (nockAction == null)
+            {
+                Debug.LogError("Cannot find nock arrow");
+            }
+        }
+        else
+        {
+            Debug.LogError("No action asset found");
+        }
+    }
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _boxCollider = GetComponent<BoxCollider>();
+        _grabInteract.trackRotation = true;
+    }
+
+    void OnEnable()
+    {
+        if (nockAction == null) return;
+        nockAction.Enable();
+        
+        nockAction.performed += OnTriggerRightPressed;
+        nockAction.canceled += OnTriggerRightReleased;
+        Debug.Log("nockAction enabled");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(_isFlying)//進行方向に回転
+        {
+            Debug.Log("flying away");
+            Vector3 velocity = transform.position - _prePosition;
+            if(velocity.magnitude > 0.01f)
+            {
+                Debug.Log("flying away");
+                transform.rotation = Quaternion.LookRotation(velocity);
+            }
+            _prePosition = transform.position;
+        }*/
+
+        if (_isNocking)
+        {
+            Vector3 direction = ArrowGrip.transform.position - this.transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            _rb.rotation = targetRotation;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            GameManager.instance.SetTargetModleMarker(collision.GetContact(0).point - collision.transform.position);
+        }
+        else
+        {
+            GameManager.instance.SetTargetModleMarker(Vector3.one * -1);
+        }
+        _isFlying = false;
+        _rb.isKinematic = true;
+        _boxCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Nock"))
+        {
+            Debug.Log(" Can Nock");
+            _canNock = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Nock"))
+        {
+            Debug.Log("Cannot Nock");
+            _canNock = false;
+            _grabInteract.trackRotation = true;
+        }
+    }
+
+    private void OnTriggerRightPressed(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("OnTriggerRightPressed");
+        if (_canNock)
+        {
+            Debug.Log("An Arrow Is Nocking");
+            _isNocking = true;
+            _grabInteract.trackRotation = false;
+            _rb.useGravity = false;
+            _isFlying = false;
+        }
+    }
+
+    private void OnTriggerRightReleased(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("OnTriggerRightReleased");
+        if (_isNocking)
+        {
+            Debug.Log("An Arrow is Shooted");
+            _isNocking = false;
+            ForceRelease();
+            yaFlyingManager.IsFlying = true;
+            _rb.useGravity = true;
+            if (ArrowGrip.transform.position != null)
+            {
+                Debug.Log($"Vector: {ArrowGrip.transform.position- this.transform.position}");
+                _rb.AddForce((ArrowGrip.transform.position - this.transform.position) * _speed, ForceMode.Impulse);
+            }
+            else
+            {
+                Debug.LogError("No Arrow Grip");
+            }
+            
+        }
+    }
+
+    private void ForceRelease()
+    {
+        var interactor = _grabInteract.interactorsSelecting.FirstOrDefault();
+        if (interactor != null)
+        {
+            _grabInteract.interactionManager.SelectExit(interactor, _grabInteract);
+        }
+            
+        Debug.Log("Force Release");
+    }
+    
+    
+}
---
+++ Assets/_VRArchery/Scripts/Equipment/YumiVRController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _VRArchery.Scripts.Equipment
+{
+    public class YumiVRController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _Ya_prefab;
+        private GameObject _Ya;
+        [SerializeField] private float _yumiMoveSpeed;
+        [SerializeField] private float _yaSpeed;
+        private Rigidbody _rb;
+        private Ya _ya;
+        [SerializeField] Transform _Gen;
+        private Vector3 _defGenPos;//弦の元の位置
+        [SerializeField] private float _lifeTime = 10;
+        [SerializeField] private float _interval = 1;
+
+        [SerializeField] private InputActionProperty _nockArrowAction;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _defGenPos = _Gen.localPosition;
+            YaInstantiate();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {         
+            if(GameManager.instance._isPlaying)
+            {
+                if (Input.GetKey(KeyCode.W) && _Ya)
+                {
+                    _Ya.transform.localPosition += new Vector3(0, 0, _yumiMoveSpeed) * Time.deltaTime;
+
+                }
+                if (Input.GetKey(KeyCode.S) && _Ya)
+                {
+                    _Ya.transform.localPosition -= new Vector3(0, 0, _yumiMoveSpeed) * Time.deltaTime;
+                }
+
+                if (_Ya)
+                {
+                    _Gen.position = _Ya.transform.position;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (_Ya)
+                    {
+                        _rb.isKinematic = false;
+                        _rb.AddForce((gameObject.transform.position - _Ya.transform.position) * _yaSpeed, ForceMode.Impulse);
+                        _Ya.transform.parent = null;
+                        _ya._isFlying = true;
+                        Destroy(_Ya, _lifeTime);
+                        _Ya = null;
+                        _rb = null;
+                        _Gen.transform.localPosition = _defGenPos;
+                        Invoke("YaInstantiate", _interval);
+                    }
+                }
+            }
+        }
+
+        private void YaInstantiate()
+        {
+            _Ya = Instantiate(_Ya_prefab,gameObject.transform);
+            _Ya.transform.position = gameObject.transform.position;
+            _Ya.transform.rotation = gameObject.transform.rotation;
+            _rb = _Ya.GetComponent<Rigidbody>();
+            _ya = _Ya.GetComponent<Ya>();
+            _rb.isKinematic = true;
+        }
+    
+    }
+}
