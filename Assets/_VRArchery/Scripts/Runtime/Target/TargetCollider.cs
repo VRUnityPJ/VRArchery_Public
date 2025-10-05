bbf70f5056bca8846115934b0b4d92bf02b97e97
nattohikoki <nattohikoki@gmail.com>
2025-10-06 01:01:42 +0900

    feat: #69 矢が的に当たったらヒットストップを起こすスクリプトを作成

--- Assets/_VRArchery/Scripts/Runtime/Target/TargetCollider.cs
+++ Assets/_VRArchery/Scripts/Runtime/Target/TargetCollider.cs
@@ -12,14 +12,21 @@ namespace _VRArchery.Scripts.Runtime.Target
     {
         [Inject]
         private ScoreHolder _scoreHolder;
+        [Tooltip("ヒットストップさせる時間（秒）")]
+        [SerializeField] private float _hitStopDuration = 0.1f;
+        
+        [Tooltip("ヒットストップ中のTime.timeScale")]
+        [SerializeField] private float _hitStopTimeScale = 0.1f;
 
         private void OnTriggerEnter(Collider other)
         {
             //敵にぶつかったとき
-            if(other.gameObject.CompareTag("Arrow"))
+            if (other.gameObject.CompareTag("Arrow"))
             {
                 _scoreHolder.AddScore(100);
+                HitStopManager.Apply(_hitStopDuration, _hitStopTimeScale);
                 Debug.Log(_scoreHolder.Score);
+                CustomDebug.Log("矢が刺さった");
             }
         }
 
