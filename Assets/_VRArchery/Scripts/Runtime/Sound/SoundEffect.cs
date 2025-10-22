cfa68bea5e7b3e130ef96f7c09d7b000e6adf822
DESKTOP-DR21MVH\favor <favorite.m.vocaloid.3250@gmail.com>
2025-10-22 20:45:49 +0900

    add: SoundEffect.cs追加

---
+++ Assets/_VRArchery/Scripts/Runtime/Sound/SoundEffect.cs
@@ -0,0 +1,20 @@
+namespace _VRArchery.Scripts.Runtime.Sound
+{
+    /// <summary>
+    /// 効果音の名前をここに登録していく
+    /// </summary>
+    public enum SoundEffect
+    {
+        Button,
+        TargetHit,
+        TargetAir,
+        CountDownShell,
+        CountDown,
+        ScrollStart,
+        ScrollEnd,
+        CRank,
+        BRank,
+        ARank,
+        SRank
+    }
+}
